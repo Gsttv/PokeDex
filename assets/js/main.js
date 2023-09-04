e7ff71ee22c5623dc0edf1f4c7171783af67f8df
Gustavo Morais <gustvmorais@gmail.com>
2023-09-04 11:30:07 -0300

    Tratando os dados recebidos da PokeAPI

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,15 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonListToHtml(pokemon) {
     return `
-        <li class="pokemon">
-                <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+                <span class="number">${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                    ${pokemon.types.map((type)=> `<li class="type">${type}</li>`).join('')}
                     </ol>
-                    <img src="images/001Bulbasaur.webp" alt=${pokemon.name} class="PokemonImg">
+                    <img src="${pokemon.photo}" alt=${pokemon.name} class="PokemonImg">
                 </div>
                 
         </li>
@@ -26,20 +22,6 @@ const pokemonList = document.getElementById('pokemonList')
 pokeAi.getPokemons().then((pokemons = []) => {
 
     pokemonList.innerHTML += pokemons.map(convertPokemonListToHtml).join('')
-    
-
-    // OS CODIGOS COMENTADO ABAIXO FAZEM A MSM COISA QUE A LINHA ACIMA
-
-    /* const newList = pokemons.map(convertPokemonListToHtml)
-       const newList = pokemons.map((pokemon) => convertPokemonListToHtml(pokemon)) // função que converte a lista em formato de json para html
-       const newHtml = newList.join('')
-       pokemonList.innerHTML += newHtml
-    */
-
-    /* for (let i = 0; i < pokemon.length; i++) {  // pecorrendo a lista e adcionadno cada elemento ao LI do HTML 
-         const pokemons = pokemon[i];
-         listItens.push(convertPokemonListToHtml(pokemons))
-     } */
 })
 
 
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,6 +1,29 @@
 
 const pokeAi = {}
 
+function covertPokeApiToPokeDetails(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeAi.getPokemonsDetails = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((responde) => responde.json())
+    .then(covertPokeApiToPokeDetails)
+
+}
+
 // metodo para retorna a lista de pokemons
 
 pokeAi.getPokemons = (offset = 0, limit = 10) => {
@@ -8,5 +31,7 @@ pokeAi.getPokemons = (offset = 0, limit = 10) => {
     return fetch(url) // metodo para fazer a requisição
         .then((response) => response.json()) // manipulando o body para um obj json
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => {console.error(error)})
+        .then((pokemons) => pokemons.map(pokeAi.getPokemonsDetails))
+        .then((detailRequest => Promise.all(detailRequest)))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
